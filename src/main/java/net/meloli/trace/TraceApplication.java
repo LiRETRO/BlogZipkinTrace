75dff3441bad8961d74020eec01533be55814c25
liretro <hash@meloli.net>
2019-10-22 00:29:09 +0800

    <未解决>升级Zipkin Server版本出错

--- src/main/java/net/meloli/trace/TraceApplication.java
+++ src/main/java/net/meloli/trace/TraceApplication.java
@@ -2,7 +2,6 @@ package net.meloli.trace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
