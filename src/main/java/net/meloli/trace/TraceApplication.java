1c2c5ca73f61c3d194123785790d4cf2a18374d2
liretro <hash@meloli.net>
2019-11-07 21:38:01 +0800

    前端统一返回过滤Null字段 实体类加入Swagger注解说明 准备实现上传服务

--- src/main/java/net/meloli/trace/TraceApplication.java
+++ src/main/java/net/meloli/trace/TraceApplication.java
@@ -2,6 +2,7 @@ package net.meloli.trace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinServer
